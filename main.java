6cdb1d16bb3db413f8966e59e09fc17c8ebfa072
daniel <danzack763@gmail.com>
2022-06-02 16:18:11 +0800

    First commit

---
+++ main.java
@@ -0,0 +1,26 @@
+/**
+ * main
+ */
+public class main {
+
+    public static void main(String[] args) {
+
+        double outputFahrenheit = CelciusToFahrenheit(11.1);
+        double outputCelcius = FahrenheitToCelcius(51.98);
+        
+        System.out.println("Celcius to Fahrenheit \n" + String.format("%.2f", outputFahrenheit));
+        System.out.println("Fahrenheit to Celcius \n" + String.format("%.2f", outputCelcius));
+    }
+
+    static double CelciusToFahrenheit(double inputCelcius) {
+        final double constDivide = 9.0/5.0;
+
+        return ((inputCelcius * (constDivide)) + 32);
+    }
+
+    static double FahrenheitToCelcius(double inputFahrenheit) {
+        final double constMultiple = 5.0/9.0;
+
+        return ((inputFahrenheit - 32) * constMultiple);
+    }
+}
